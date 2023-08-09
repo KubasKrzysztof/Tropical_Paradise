f3f7d2b26bba9ad8ee541b32a0750265bd04d151
Krzysztof Kubas <kubas.krzysztof@gmail.com>
2023-08-09 23:21:29 +0200

    Navi cz 3

--- js/main.js
+++ js/main.js
@@ -10,6 +10,18 @@ const handleNav = () => {
 			nav.classList.remove('nav--active')
 		})
 	})
+	handelNavItemAnimation()
+}
+
+const handelNavItemAnimation = () => {
+	let delayTime = 1
+
+	allNavItem.forEach(item => {
+		item.classList.toggle('nav-item-animation')
+
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
 }
 
 navBtn.addEventListener('click', handleNav)
